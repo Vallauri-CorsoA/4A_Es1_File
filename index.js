d9aa3fd3c6bd452321126de8c638fb93e4edf645
Daniele Fea <daniele.fea@Vallauri.Net>
2023-10-12 13:23:04 +0200

    Terminata la lettura e spiegazione della lettura di file locali e accenna la fetch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var _btnCarica, _txtFile;
+var _btnCarica, _btnCarica2, _txtFile;
 
 window.onload= function(){
     //Contesto: window -> perché function() è richiamata da window
@@ -6,6 +6,7 @@ window.onload= function(){
 
 
     _btnCarica = document.querySelector("button");
+    _btnCarica2 = document.getElementById("btnCarica2");
     _txtFile = document.querySelector("input[type=file]");
 
     //leggiFile() è richiamata in modo SINCRONO
@@ -13,10 +14,18 @@ window.onload= function(){
 
     //leggiFile è richiamata in modo ASINCRONO dal bottone
     //.bind(contesto, eventuali parametri presenti nella funzione)
-    _btnCarica.addEventListener("click", leggiFile.bind(_txtFile, "TEST"));
+    _btnCarica.addEventListener("click", leggiFile1.bind(_txtFile, "TEST"));
+    _btnCarica2.addEventListener("click", leggiFile2);
 };
 
-function leggiFile(testo){
+function leggiFile2(){
+    //Prendere file, mandare e ricevere info sul server
+    //ASINCRONA
+    let dati = fetch("dati.json");
+    console.log(dati);
+}
+
+function leggiFile1(testo){
     //SENZA BIND -> this è uguale al bottone 
     console.log(this);
     console.log(testo);
@@ -25,8 +34,11 @@ function leggiFile(testo){
 
     //Istanzio la classe FileReader
     let reader = new FileReader();
+    //SINCRONIZZO Flusso principale con quello secondario 
+    //avvia da readAsDataURL tramite callback
     reader.onload = fineLettura;
-    //
+    //E' una chiamata SINCRONA readAsDataURL che nasconde al suo interno 
+    //una chiamata di sistema ASINCRONA
     reader.readAsDataURL(this.files[0]);
     alert("Lettura avviata");
 }
@@ -35,7 +47,10 @@ function fineLettura(e){
     console.log(e);
     alert("file letto veramente");
     
-    //let datiFile = e.target.result;
-    //let stringa = datiFile.split(",")[1];
-    //alert(atob(stringa));
+    //Prendo i dati
+    let datiFile = e.target.result;
+    //Separo i dati dall'intestazione
+    let stringa = datiFile.split(",")[1];
+    //Decodificare i dati in formato base 64
+    alert(atob(stringa));
 }
